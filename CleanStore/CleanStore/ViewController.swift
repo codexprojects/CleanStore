bf963b76299be3b69841e4edb03b8a496dc06dc4
Ilke Yucel <ilke.yucel@vesium.com>
2021-12-02 22:58:49 +0300

    Clean swift project template initial commit.

---
+++ CleanStore/CleanStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by Ilke Yucel on 2.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
